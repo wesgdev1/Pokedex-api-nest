3e2adefc3413ee7f5f5f45450db131fff7fe7deb
Welinton Suarez <wesgdeveloper@gmail.com>
2024-05-17 16:22:48 -0500

    handle Pokemon creation errors

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -3,16 +3,32 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 export class PokemonService {
+  // usamos el InjectModel para inyectar el modelo de Pokemon
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
   ) {}
 
-  create(createPokemonDto: CreatePokemonDto) {
+  async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleUpperCase();
-    return createPokemonDto;
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(
+          `Pokemon already exists - ${JSON.stringify(error.keyValue)}`,
+        );
+      }
+      console.log(error);
+      throw new InternalServerErrorException();
+    }
   }
 
   findAll() {
